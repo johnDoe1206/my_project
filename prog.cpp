03269df9f49ca8bb3a31c7226632d440b43d5c71
Kirill <coffiplay228@gmail.com>
2025-05-27 18:48:42 +0300

    Added difference output

--- prog.cpp
+++ prog.cpp
@@ -1,4 +1,3 @@
-// prog.cpp
 #include "sdt.h"
 #include <iostream>
 
@@ -6,6 +5,12 @@ int main() {
     std::cout << "Enter two integers: ";
     int a, b;
     std::cin >> a >> b;
+    
+    // Вывод суммы (уже было)
     std::cout << "Sum: " << a + b << std::endl;
+    
+    // Добавьте вывод разности
+    std::cout << "Difference: " << a - b << std::endl;
+    
     return 0;
 }
